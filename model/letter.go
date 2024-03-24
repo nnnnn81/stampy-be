68f526c651668dba54a9e7a89146e5320cdf728b
nnnnn81 <nnnnnagi81@gmail.com>
2024-03-24 19:22:15 +0900

    fix: models

--- model/letter.go
+++ model/letter.go
@@ -2,18 +2,17 @@ package model
 
 import "time"
 
-type Notice struct {
+type Letter struct {
 	Id         uint      `gorm:"primary_key" json:"id"`
 	Type       string    `json:"type"`
 	Title      string    `json:"title"`
 	Stamp      string    `json:"stamp"`
 	Content    string    `json:"content"`
-	CurrentDay int       `json:"currentDay"`
-	IsLastDay  bool      `json:"isLastDay"`
 	HrefPrefix string    `json:"hrefPrefix"`
 	Sender     uint      `json:"sender"`
 	Receiver   uint      `json:"receiver"`
 	Read       bool      `json:"read"`
+	IsVisible  bool      `json:"isVisible"`
 	CreatedAt  time.Time `json:"createdAt"`
 	ListType   string    `json:"listType"`
 	CardId     uint      `json:"cardId"`
